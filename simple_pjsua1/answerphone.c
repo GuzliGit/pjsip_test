70352e3caa514f777a58dbbbd357b22211d3720d
GuzliGit <badindan2003@gmail.com>
2025-10-27 13:50:00 +0700

    Implemented sending: continuous audio signal, audio message from wav file, RBT imitation | To do: Support for processing multiple calls

--- simple_pjsua1/answerphone.c
+++ simple_pjsua1/answerphone.c
@@ -2,6 +2,13 @@
 #include <signal.h>
 #include <pjsua-lib/pjsua.h>
 
+typedef enum response_mode
+{
+    CONTINUOUS_SIGNAL,
+    FROM_WAV_SIGNAL,
+    RBT_SIGNAL
+} response_mode;
+
 static pj_caching_pool ch_pool;
 static pj_pool_t* main_pool;
 static pj_timer_heap_t* t_heap;
@@ -14,7 +21,7 @@ static pjsua_logging_config log_cfg;
 static pjsua_transport_config t_cfg;
 static pjsua_acc_config acc_cfg;
 
-static int send_tone(pjsua_call_id);
+static int send_media(pjsua_call_id, response_mode, unsigned int, const char*);
 static int create_transport();
 static int create_pools();
 static int create_timer_heap();
@@ -37,20 +44,21 @@ static void signal_handler()
 
 static void answer_timer_callback(pj_timer_heap_t* timer_heap, pj_timer_entry* entry)
 {
+    pj_status_t status;
     pjsua_call_id call_id = (pjsua_call_id)(long)entry->user_data;
 
     PJ_LOG(1, (__FILE__, "==Answer_timer_callback=="));
-    pjsua_call_answer(call_id, 200, NULL, NULL);
-    
-    if (send_tone(call_id) != PJ_SUCCESS)
+    status = pjsua_call_answer(call_id, 200, NULL, NULL);
+    if (status != PJ_SUCCESS)
     {
-        PJ_LOG(1, (__FILE__, "pjsua_call_answer error (200)"));
+        PJ_LOG(1, (__FILE__, "==pjsua_call_answer error (200)=="));
+        return;
     }
-}
-
-static void timer_callback(pj_timer_heap_t* timer_heap, pj_timer_entry* entry)
-{
-    PJ_LOG(1, (__FILE__, "==Timer_callback=="));
+    
+    if (send_media(call_id, FROM_WAV_SIGNAL, 2000, DEFAULT_WAV_PATH) != PJ_SUCCESS)
+    {
+        PJ_LOG(1, (__FILE__, "==send_media error=="));
+    }
 }
 
 static void on_incoming_call(pjsua_acc_id acc_id, pjsua_call_id call_id, pjsip_rx_data* rdata)
@@ -70,13 +78,24 @@ static void on_incoming_call(pjsua_acc_id acc_id, pjsua_call_id call_id, pjsip_r
 
     PJ_LOG(3, (__FILE__, "Call %d state=%.*s", call_id, (int)info.state_text.slen, info.state_text.ptr));
 
-    pjsua_call_answer(call_id, 180, NULL, NULL);
+    status = pjsua_call_answer(call_id, 180, NULL, NULL);
+    if (status != PJ_SUCCESS)
+    {
+        PJ_LOG(1, (__FILE__, "==pjsua_call_answer error (180)=="));
+        return;
+    }
 
     pj_timer_entry* timer = pj_pool_zalloc(main_pool, sizeof(pj_timer_entry));
-    pj_time_val delay = {3, 100};
+    if (!timer)
+    {
+        PJ_LOG(1, (__FILE__, "==pj_pool_zalloc for timer error=="));
+        return;
+    }
+
     pj_timer_entry_init(timer, PJSUA_INVALID_ID, NULL, &answer_timer_callback);
     timer->user_data = (void*)(long)call_id;
-
+    
+    pj_time_val delay = {3, 100};
     status = pj_timer_heap_schedule(t_heap, timer, &delay);
     if (status != PJ_SUCCESS)
     {
@@ -85,7 +104,7 @@ static void on_incoming_call(pjsua_acc_id acc_id, pjsua_call_id call_id, pjsip_r
     }
 }
 
-static int send_tone(pjsua_call_id call_id)
+static int send_media(pjsua_call_id call_id, response_mode call_mode, unsigned int duration, const char* filename)
 {
     pj_status_t status;
     char temp[16];
@@ -99,18 +118,47 @@ static int send_tone(pjsua_call_id call_id)
         return -1;
     }
 
-    pjmedia_port* tone_port = pj_pool_alloc(media_pool, sizeof(pjmedia_port));
-    status = pjmedia_tonegen_create(media_pool, PJSUA_DEFAULT_CLOCK_RATE, 1, PJSUA_CALL_SEND_DTMF_DURATION_DEFAULT, 16, 0, &tone_port);
-    if (status != PJ_SUCCESS)
+    pjmedia_port* media_port = pj_pool_zalloc(media_pool, sizeof(pjmedia_port));
+    if (!media_port)
     {
-        PJ_LOG(1, (__FILE__, "==pjmedia_tonegen_create error=="));
+        PJ_LOG(1, (__FILE__, "==pj_pool_zalloc for media_port error=="));
+        pj_pool_release(media_pool);
+        pjsua_call_hangup(call_id, 486, NULL, NULL);
+        return -1;
+    }
+
+    if (call_mode == CONTINUOUS_SIGNAL || call_mode == RBT_SIGNAL)
+    {
+        status = pjmedia_tonegen_create(media_pool, PJSUA_DEFAULT_CLOCK_RATE, 1, PJSUA_CALL_SEND_DTMF_DURATION_DEFAULT, 16, 0, &media_port);
+        if (status != PJ_SUCCESS)
+        {
+            PJ_LOG(1, (__FILE__, "==pjmedia_tonegen_create error=="));
+            pj_pool_release(media_pool);
+            pjsua_call_hangup(call_id, 486, NULL, NULL);
+            return -1;
+        }
+    }
+    else if (call_mode == FROM_WAV_SIGNAL)
+    {
+        status = pjmedia_wav_player_port_create(media_pool, filename, 0, 0, 0, &media_port);
+        if (status != PJ_SUCCESS)
+        {
+            PJ_LOG(1, (__FILE__, "==pjmedia_wav_player_port_create error=="));
+            pj_pool_release(media_pool);
+            pjsua_call_hangup(call_id, 486, NULL, NULL);
+            return -1;
+        }
+    }
+    else
+    {
+        PJ_LOG(1, (__FILE__, "==Can't find such response mode: %d==", call_mode));
         pj_pool_release(media_pool);
         pjsua_call_hangup(call_id, 486, NULL, NULL);
         return -1;
     }
 
     pjsua_conf_port_id conf_slot;
-    status = pjsua_conf_add_port(media_pool, tone_port, &conf_slot);
+    status = pjsua_conf_add_port(media_pool, media_port, &conf_slot);
     if (status != PJ_SUCCESS)
     {
         PJ_LOG(1, (__FILE__, "==pjsua_conf_add_port error=="));
@@ -120,9 +168,25 @@ static int send_tone(pjsua_call_id call_id)
     }
 
     status = pjsua_set_null_snd_dev();
+    if (status != PJ_SUCCESS)
+    {
+        PJ_LOG(1, (__FILE__, "==pjsua_set_null_snd_dev error=="));
+        pjsua_conf_remove_port(conf_slot);
+        pj_pool_release(media_pool);
+        pjsua_call_hangup(call_id, 486, NULL, NULL);
+        return -1;
+    }
 
     pjsua_call_info call_info;
     status = pjsua_call_get_info(call_id, &call_info);
+    if (status != PJ_SUCCESS)
+    {
+        PJ_LOG(1, (__FILE__, "==pjsua_call_get_info error=="));
+        pjsua_conf_remove_port(conf_slot);
+        pj_pool_release(media_pool);
+        pjsua_call_hangup(call_id, 486, NULL, NULL);
+        return -1;
+    }
 
     status = pjsua_conf_connect(conf_slot, call_info.conf_slot);
     if (status != PJ_SUCCESS)
@@ -133,16 +197,34 @@ static int send_tone(pjsua_call_id call_id)
         pjsua_call_hangup(call_id, 486, NULL, NULL);
         return -1;
     }
-    PJ_LOG(3, (__FILE__, "Tone 425Hz started for call %d, conf_slot=%d", call_id, conf_slot));
 
-    pjmedia_tone_desc tone;
-    tone.freq1 = 425;
-    tone.freq2 = 0;
-    tone.off_msec = 0;
-    tone.on_msec = 10000;
+    switch (call_mode)
+    {
+        case FROM_WAV_SIGNAL:
+        {
+            pj_thread_sleep(duration);
+            break;
+        }
 
-    status = pjmedia_tonegen_play(tone_port, 1, &tone, PJMEDIA_TONEGEN_LOOP);
-    pj_thread_sleep(tone.on_msec);
+        case CONTINUOUS_SIGNAL:
+        case RBT_SIGNAL: 
+        {
+            PJ_LOG(3, (__FILE__, "Tone 425Hz started for call %d | call_mode=%d, conf_slot=%d", call_id, call_mode, conf_slot));
+
+            pjmedia_tone_desc tone;
+            tone.freq1 = 425;
+            tone.freq2 = 0;
+            tone.off_msec = call_mode == CONTINUOUS_SIGNAL ? 0 : 4000;
+            tone.on_msec = 1000;
+
+            status = pjmedia_tonegen_play(media_port, 1, &tone, PJMEDIA_TONEGEN_LOOP);
+            if (status == PJ_SUCCESS)
+            {
+                pj_thread_sleep(duration);
+            }
+            break;
+        }
+    }
 
     pjsua_conf_disconnect(conf_slot, call_info.conf_slot);
     pjsua_conf_remove_port(conf_slot);
@@ -158,14 +240,16 @@ int init_answerphone()
     status = pjsua_create();
     if (status != PJ_SUCCESS)
     {
-        printf("pjsua_create error\n");
+        PJ_LOG(1, (__FILE__, "==pjsua_create error=="));
         return -1;
     }
 
     pjsua_config_default(&cfg);
+    cfg.max_calls = MAX_CALLS;
     cfg.cb.on_incoming_call = &on_incoming_call;
 
     pjsua_media_config_default(&med_cfg);
+    med_cfg.max_media_ports = MAX_CALLS;
 
     pjsua_logging_config_default(&log_cfg);
     log_cfg.msg_logging = PJ_TRUE;
@@ -174,7 +258,7 @@ int init_answerphone()
     status = pjsua_init(&cfg, &log_cfg, &med_cfg);
     if (status != PJ_SUCCESS)
     {
-        printf("pjsua_init error\n");
+        PJ_LOG(1, (__FILE__, "==pjsua_init error=="));
         pjsua_destroy();
         return -1;
     }
@@ -212,7 +296,7 @@ static int create_transport()
     status = pjsua_transport_create(PJSIP_TRANSPORT_UDP, &t_cfg, NULL);
     if (status != PJ_SUCCESS)
     {
-        printf("pjsua_transport_create error\n");
+        PJ_LOG(1, (__FILE__, "==pjsua_transport_create error=="));
     }
 
     return status;
@@ -224,7 +308,7 @@ static int create_pools()
     main_pool = pj_pool_create(&ch_pool.factory, "main", MAIN_POOL_SIZE, MAIN_POOL_SIZE, NULL);
     if (!main_pool)
     {
-        printf("pj_pool_create error\n");
+        PJ_LOG(1, (__FILE__, "==pj_pool_create error=="));
         return -1;
     }
 
@@ -236,7 +320,7 @@ static int create_timer_heap()
     pj_status_t status = pj_timer_heap_create(main_pool, 10, &t_heap);
     if (status != PJ_SUCCESS)
     {
-        printf("pj_timer_heap_create error\n");
+        PJ_LOG(1, (__FILE__, "==pj_timer_heap_create error=="));
     }
 
     return status;
@@ -272,7 +356,7 @@ static int add_account(const char* sip_user, const char* sip_domain)
     status = pjsua_acc_add(&acc_cfg, PJ_TRUE, &acc_id);
     if (status != PJ_SUCCESS)
     {
-        printf("pjsua_acc_add error\n");
+        PJ_LOG(1, (__FILE__, "==pjsua_acc_add error=="));
     }
 
     return status;
@@ -292,7 +376,7 @@ int start_answerphone(const char* sip_user, const char* sip_domain)
     status = pjsua_start();
     if (status != PJ_SUCCESS)
     {
-        printf("pjsua_start error\n");
+        PJ_LOG(1, (__FILE__, "==pjsua_start error=="));
         cleanup_resources();
         return -1;
     }
--- simple_pjsua1/answerphone.h
+++ simple_pjsua1/answerphone.h
@@ -1,12 +1,16 @@
 #ifndef ANSWERPHONE_H
 #define ANSWERPHONE_H
 
-#define DEFAULT_PASSWD "passwd"
 #define MAIN_POOL_SIZE 128
 #define MEDIA_POOL_SIZE 512
 #define MEDIA_POOL_INC_SIZE 128
+
 #define MAX_USERNAME_LEN 32
 #define MAX_DOMAIN_LEN 64
+#define MAX_CALLS 20
+
+#define DEFAULT_PASSWD "passwd"
+#define DEFAULT_WAV_PATH "./media/test.wav"
 
 int init_answerphone();
 
